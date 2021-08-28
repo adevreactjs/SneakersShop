b81c2872cfebc223575c4b8c94f2208c988b8357
adevreactjs <adevreactjs@gmail.com>
2021-08-29 01:52:03 +0300

    add cart

--- src/App.js
+++ src/App.js
@@ -5,16 +5,44 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
-          <div class="cartItem d-flex align-center">
+          <h2 className="d-flex justify-between mb-30">Корзина<img className="remove-btn cu-p" src="/img/btn-delete.svg" alt="img"/></h2>
+
+          <div className="cartItem d-flex align-center">
             <img className="mr-20" width={70} height={70} src="/img/cross.jpg" alt="img"/>
-            <div className="mr-20">
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+            <div className="">
+              <p className="">Мужские Кроссовки Nike Air Max 270</p>
               <b>12 999 руб.</b>
             </div>
-            <img src="/img/btn-delete.svg" alt="img"/>
-
+            <img className="remove-btn" src="/img/btn-delete.svg" alt="img"/>
           </div>
+
+          <div className="cartItem d-flex align-center">
+            <img className="mr-20" width={70} height={70} src="/img/cross.jpg" alt="img"/>
+            <div className="">
+              <p className="">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 руб.</b>
+            </div>
+            <img className="remove-btn" src="/img/btn-delete.svg" alt="img"/>
+          </div>
+
+          <div className="items">
+            <ul className="cartTotalBlock">
+               <li className="d-flex mb-20">
+                 <span>Итого: </span>
+                 <div></div>
+                 <b>21 498 руб. </b>
+               </li>
+               <li className="d-flex">
+                 <span>Налог 5%: </span>
+                 <div></div>
+                 <b>1074 руб. </b>
+               </li>
+               <button className="greenButton">Оформить заказ <img src="/img/arrow.svg" alt="arrow"/> </button>
+
+            </ul>
+            
+          </div>
+
         </div>
       </div>
 
